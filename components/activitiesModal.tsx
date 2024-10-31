46f91242b4b58ba40228e2bb2c6c73036bc2dd60
Luis Modesto <51082234+LuisFelipeMod@users.noreply.github.com>
2024-10-31 18:36:14 -0300

    feat: activities modal component

---
+++ components/activitiesModal.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import {
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  Button,
+  useDisclosure,
+  Select,
+  SelectItem,
+  Textarea,
+} from "@nextui-org/react";
+
+interface Data {
+  data: {
+    customers: string[][] | null | undefined;
+  };
+}
+
+export default function ActivitiesModal(data: Data) {
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  const customers = data.data.customers;
+  let counter = 0;
+
+  customers?.map((e) => {
+    console.log(e);
+  });
+
+  return (
+    <div className="flex gap-3">
+      <Button onPress={onOpen} color="primary">
+        Open Modal
+      </Button>
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange} placement="top-center">
+        <ModalContent>
+          {(onClose) => (
+            <>
+              <ModalHeader className="flex flex-col gap-1">
+                Iniciar contador de atividade
+              </ModalHeader>
+              <ModalBody>
+                <Select isRequired label="Atividade/Projeto">
+                  {customers ? (
+                    customers.map((customersName) => {
+                      return <SelectItem key={customersName[0]} >{customersName[0]}</SelectItem>;
+                    })
+                  ) : (
+                    <></>
+                  )}
+                </Select>
+                <Textarea
+                  isRequired
+                  label="Descricao da atividade"
+                  placeholder="Escreva a descricao"
+                />
+              </ModalBody>
+              <ModalFooter>
+                <Button color="primary" onPress={onClose}>
+                  Iniciar
+                </Button>
+              </ModalFooter>
+            </>
+          )}
+        </ModalContent>
+      </Modal>
+    </div>
+  );
+}
