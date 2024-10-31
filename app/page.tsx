3082b7c7f4d7b3de38917e8df4b86665d1c7f5af
Luis Modesto <51082234+LuisFelipeMod@users.noreply.github.com>
2024-10-31 18:34:36 -0300

    feat: home page + fetch sheet data

--- app/page.tsx
+++ app/page.tsx
@@ -1,56 +1,53 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
 
-import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+import ActivitiesModal from "@/components/activitiesModal"
 
-export default function Home() {
+import { google } from "googleapis";
+
+async function fetchSheet(method:string, range: string){
+  const auth = await google.auth.getClient({ scopes: ["https://www.googleapis.com/auth/spreadsheets.readonly"] });
+  const sheets = google.sheets({version: "v4", auth});
+
+  if (method === "GET"){
+    const response = await sheets.spreadsheets.values.get({
+      spreadsheetId: process.env.GOOGLE_SHEET_ID,
+      range
+    })
+
+    const values = response.data.values 
+
+    values?.shift()
+
+    return values
+  }
+
+  return
+}
+ 
+
+
+export default async function Home() {
+
+  interface Data {
+    customers: string[][] | null | undefined
+  }
+
+  const data:Data = {
+    customers: await fetchSheet('GET', `Clientes!A:A`),
+
+  }
+
   return (
     <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10">
       <div className="inline-block max-w-xl text-center justify-center">
-        <span className={title()}>Make&nbsp;</span>
-        <span className={title({ color: "violet" })}>beautiful&nbsp;</span>
+        <span className={title()}>Registre</span>
+        <span className={title({ color: "blue" })}> suas atividades</span>
         <br />
-        <span className={title()}>
-          websites regardless of your design experience.
-        </span>
-        <div className={subtitle({ class: "mt-4" })}>
-          Beautiful, fast and modern React UI library.
-        </div>
+        <span className={title()}>aqui</span>
       </div>
 
-      <div className="flex gap-3">
-        <Link
-          isExternal
-          className={buttonStyles({
-            color: "primary",
-            radius: "full",
-            variant: "shadow",
-          })}
-          href={siteConfig.links.docs}
-        >
-          Documentation
-        </Link>
-        <Link
-          isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.links.github}
-        >
-          <GithubIcon size={20} />
-          GitHub
-        </Link>
-      </div>
+    <ActivitiesModal data={data}/>
 
-      <div className="mt-8">
-        <Snippet hideCopyButton hideSymbol variant="bordered">
-          <span>
-            Get started by editing <Code color="primary">app/page.tsx</Code>
-          </span>
-        </Snippet>
-      </div>
     </section>
   );
 }
